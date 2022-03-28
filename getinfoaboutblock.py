442305eb03140a6166d7699f3ed3afdbe90328b4
Platon <93481486+Platonwrld@users.noreply.github.com>
2022-03-28 23:56:35 +0300

    Update getinfoaboutblock.py

--- getinfoaboutblock.py
+++ getinfoaboutblock.py
@@ -2,12 +2,9 @@ from web3 import Web3
 
 
 # read information from the blockchain
-infura_url = "https://mainnet.infura.io/v3/ef0e860f921a488fa3d889927970f4a9"    # ссылка на endpoint проекта на infura
-web3 = Web3(Web3.HTTPProvider(infura_url))                                      # установление взаимодейтсвия проекта и сервера 
-  
-
-#latest_block = web3.eth.blockNumber                                            
-#print(web3.eth.getBlock(latest_block))                                         # print all information about block
+infura_url = ""                                                                 # reference on endpoint from infura
+web3 = Web3(Web3.HTTPProvider(infura_url))                                      
+                                        
 
 hash = '0x25433d44ebf7dfca8d82597a782d94e51ace4d6c5635cb4d55f470d97b32d8de'     # hash by block
 print(web3.eth.getTransactionByBlock(hash, 2))                                  # 2 - transaction that i wanna see
